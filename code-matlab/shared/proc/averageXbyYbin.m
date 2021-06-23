3f3f7e2ac3754cf45d4d8f5a9ba77b6ed0b9e138
mvdm <mvdm@dartmouth.edu>
2021-06-23 12:38:13 -0400

    Expanded help for averageXbyYbin()

--- code-matlab/shared/proc/averageXbyYbin.m
+++ code-matlab/shared/proc/averageXbyYbin.m
@@ -1,5 +1,19 @@
 function x_avg = averageXbyYbin(x,y,y_edges)
-%function x_avg = averageXbyYbin(x,y,y_edges)
+% function x_avg = averageXbyYbin(x,y,y_edges)
+%
+% Bins variable y into bins defined by y_edges; then for each y-bin,
+% computes the average of x. Inputs x and y must have the same length, 
+% and each element is assumed to correspond to the same time.
+% (note you can use interp1() to make them the same length)
+%
+% This works by finding the indices of y that go into each y-bin, and
+% then for each bin averaging the x-values corresponding to those idxs.
+%
+% Example: you have firing rate as a function of time, and you have
+% position as a function of time. Get the average firing rate for each
+% position bin:
+%
+% fr_avg_bin = averageXbyYbin(fr, pos, pos_edges);
 
 [~,idx] = histc(y,y_edges);
 
